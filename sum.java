6ab2aae6028ddda0e324efdfe57a52af6e95c611
Baby Yadav <yadavbaby4141@gmail.com>
2021-07-08 13:19:49 +0530

    added

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum{
     public static void main(String[] args){
         int a=5;
         int b=10;
-        System.out.println(a+b);
+        System.out.println("substract"a-b);
 
     }
 }
